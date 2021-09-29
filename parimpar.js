5e91a985214bb78b0c18283d83dd5dd3ff349e04
lidiapanis1 <89846034+lidiapanis1@users.noreply.github.com>
2021-09-28 21:15:21 -0300

    Add files via upload
    Par e impar criado em conjunto com a Suellen

---
+++ parimpar.js
@@ -0,0 +1,15 @@
+
+
+var numalunos = 0;
+
+for (index = 0; index < 10; index ++) {  
+         if (numalunos == 0){
+        console.log ("aluno numero zero:" +numalunos);
+         } else if (numalunos & 1) {
+        console.log("alunos impar: "+numalunos);
+        } else {
+        console.log("alunos par: "+numalunos);
+        }
+        numalunos ++;
+        }
+    
